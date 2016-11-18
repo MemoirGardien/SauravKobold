dbd2231829004a0416ea7a5787a14a4db3181ddd
rnort003 <rnort003@plattsburgh.edu>
2016-11-17 20:20:56 -0500

    Update checksum.py

--- checksum.py
+++ checksum.py
@@ -2,23 +2,22 @@ import hashlib
 
 # File to check
 fname = input("Enter file name: ")
-
-
 # this gets the md5 checksum of the original file
-original_md5 = hashlib.md5(open(fname ,'rb').read()).hexdigest()
-print (original_md5)
-
+md5a = hashlib.md5(open(fname ,'rb').read()).hexdigest()
+print (md5a)
+#---------------------------------------------------------------------
+#gets file to check against
 checkfile = input('Enter file name to check contents against: ')
-
-# Open,close, read file and calculate MD5 on its contents
-with open(checkfile, 'rb') as file_to_check:
-    # read contents of the file
-    data = file_to_check.read()
-    # get the md5 checksum of the new file
-    md5_returned = hashlib.md5(data).hexdigest()
-
-# comparing the original MD5 with freshly calculated md5
-if original_md5 == md5_returned:
+#open file
+opencf = open(checkfile, 'rb')
+#read the contents of the file
+data = opencf.read()
+#create md5 checksum of file
+md5b = hashlib.md5(data).hexdigest()
+print(md5b)
+#---------------------------------------------------------------------
+# comparing the original md5 with new md5
+if md5a == md5b:
     print("Checksum IS correct.")
 else:
     print ("Checksum IS NOT correct.")
